df93ca1370c40f082f7c4c6102a6cc24f2774b18
Dilan Ramos Flores <ramosfloresangielodilan@gmail.com>
2025-10-22 22:00:03 -0500

    first commit

---
+++ src/coffeandtea/CoffeAndTea.java
@@ -0,0 +1,12 @@
+package coffeandtea;
+
+import javax.swing.JOptionPane;
+
+public class CoffeAndTea {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+    }
+
+}
